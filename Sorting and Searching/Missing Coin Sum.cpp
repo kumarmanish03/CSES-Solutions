d7069957d114ce1f8f82f5ab24dc45e0a6deafe3
kumarmanish03 <manish15052000@gmail.com>
2022-04-22 15:25:40 +0530

    Good

---
+++ Sorting and Searching/Missing Coin Sum.cpp
@@ -0,0 +1,50 @@
+/*
+* 
+* @author: Manish Kumar
+* @institute: IIIT Allahabad, INDIA
+*
+* @problem: https://cses.fi/problemset/task/2183
+*
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+void print(T &&t)  { cout << t << "\n"; }
+template <typename T, typename... Args>
+void print(T &&t, Args &&... args)
+{   cout << t << " ", print(forward<Args>(args)...); }
+#define _print(v) for(auto i:(v)) cout<<i<<" "; cout<<"\n";
+#define fs  first
+#define sc  second
+#define pb  push_back
+#define int long long
+
+int solve(){
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for(int &x:v)
+        cin >> x;
+    
+    sort(v.begin(), v.end());
+
+    int res = 1;
+    for(int i = 0; i < n && res >= v[i]; i++)
+      res += v[i];
+
+    print(res);
+    
+    return 0;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int t = 1;
+    // cin>>t;
+    for(int i = 0; i < t; i++)
+        solve();
+}
