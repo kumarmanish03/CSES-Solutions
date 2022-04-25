2e62bce8cf8d12d48aa4f54b13281d5399c0f01c
kumarmanish03 <manish15052000@gmail.com>
2022-04-25 22:54:02 +0530

    Easy, Observation

---
+++ Sorting and Searching/Reading Books.cpp
@@ -0,0 +1,57 @@
+/*
+* 
+* @author: Manish Kumar
+* @institute: IIIT Allahabad, INDIA
+*
+* @problem: https://cses.fi/problemset/task/1631/
+* @disc: observation
+*
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+void print(T &&t)  { cout << t << "\n"; }
+template <typename T, typename... Args>
+void print(T &&t, Args &&... args)
+{   cout << t << " ", print(forward<Args>(args)...); }
+#define _print(v) for(auto i:(v)) cout<<i<<" "; cout<<"\n";
+#define fs  first
+#define sc  second
+#define pb  push_back
+#define int long long
+
+int solve(){
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    int total = 0;
+    for(int &x:v){
+        cin >> x;
+        total += x;
+    }
+    
+    int mx = *max_element(v.begin(), v.end());
+    int rem = total - mx;
+
+    if(mx > rem){
+        print(2*mx);
+    }
+    else{
+        print(total);
+    }
+
+    
+    return 0;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int t = 1;
+    // cin>>t;
+    for(int i = 0; i < t; i++)
+        solve();
+}
