9cf8e2cdef00e969dd4535c8812657058c49e23b
kumarmanish03 <manish15052000@gmail.com>
2022-05-15 14:36:15 +0530

    Very Good Question, Must try

--- Tree Algorithms/Tree Distances I.cpp
+++ Tree Algorithms/Tree Distances I.cpp
@@ -5,12 +5,10 @@
 *
 * @problem: https://cses.fi/problemset/task/1132/
 * @refer: https://www.youtube.com/watch?v=Rnv4qvoxsTo&ab_channel=HiteshTripathi
-* @TC: O(V + E)
 * @desc: 
 *  # First we are calulating distance to all nodes from a random node(ex 1);
 *  # Maximum distance node is surely be one of the diameter node
 *  # So we got one of the diameter node which is 'node1'
-*  # reset all variables i.e, vis, dis
 *  # Similarly we call dfs from node1 and then maxminum distance node from this is surely be another diameter node i.e, 'node2'
 *  # A Good Observation for this problem is that: for each node its maximum distance is from one of the diameter nodes of this tree
 *  # So we calculate distance of all nodes from each of diameter nodes
@@ -32,16 +30,13 @@ void print(T &&t, Args &&... args)
 
 const int N = 2e5+5;
 
-bool vis[N];
-
 // calculates distance of all nodes from node it is called and saves distance in 'dis' vector
-void dfs(int u, int d, vector<int>& dis, vector<vector<int>> &adj){
+void dfs(int u, int d, int p, vector<int>& dis, vector<vector<int>> &adj){
 
-    vis[u] = true;
     dis[u] = d;
     for(auto v: adj[u]){
-        if(vis[v]) continue;
-        dfs(v, d+1, dis, adj);
+        if(v == p) continue;
+        dfs(v, d+1, u, dis, adj);
     }
 }
 
@@ -59,9 +54,8 @@ int solve(){
     vector<int> dis;
 
     // Finding one of the diameter node 'node1'
-    memset(vis, false, sizeof(vis));
     dis.resize(n+1, 0);
-    dfs(1, 0, dis, adj);
+    dfs(1, 0, -1, dis, adj);
     int node1_dis = 0;
     int node1 = 0;
     for(int u = 1; u <= n; u++){
@@ -72,9 +66,8 @@ int solve(){
     }
 
     // Using 'node1' finding another diameter node 'node2'
-    memset(vis, false, sizeof(vis));
     dis.resize(n+1, 0);
-    dfs(node1, 0, dis, adj);
+    dfs(node1, 0, -1, dis, adj);
     int node2_dis = 0;
     int node2 = 0;
     for(int u = 1; u <= n; u++){
@@ -87,10 +80,8 @@ int solve(){
     // Now we got node1 & node2
     vector<int> d1(n+1, 0); // d1[u]: stores distance of node u from node1
     vector<int> d2(n+1, 0); // d2[u]: stores distance of node u from node2
-    memset(vis, false, sizeof(vis));
-    dfs(node1, 0, d1, adj);
-    memset(vis, false, sizeof(vis));
-    dfs(node2, 0, d2, adj);
+    dfs(node1, 0, -1, d1, adj);
+    dfs(node2, 0, -1, d2, adj);
 
     for(int i = 1; i <= n; i++){
         cout << max(d1[i], d2[i]) << " ";
