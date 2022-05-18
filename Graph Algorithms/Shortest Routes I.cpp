1e73c84ea74c9b7ed4e4a70ff3e7d28f66dbc1c9
kumarmanish03 <manish15052000@gmail.com>
2022-05-18 20:52:08 +0530

    Dijkstra Algorithm

---
+++ Graph Algorithms/Shortest Routes I.cpp
@@ -0,0 +1,85 @@
+/*
+* 
+* @author: Manish Kumar
+* @institute: IIIT Allahabad, INDIA
+*
+* @problem: https://cses.fi/problemset/task/1671/
+* @refer: https://www.youtube.com/watch?v=F3PNsWE6_hM&t=874s&ab_channel=Luv
+* @TC: O(V + ElogV) [for adj. list]
+* @desc: Dijkstra Algorithm
+*
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+void print(T &&t)  { cout << t << "\n"; }
+template <typename T, typename... Args>
+void print(T &&t, Args &&... args)
+{   cout << t << " ", print(forward<Args>(args)...); }
+#define _print(v) for(auto i:(v)) cout<<i<<" "; cout<<"\n";
+#define fs  first
+#define sc  second
+#define pb  push_back
+#define int long long
+
+#define INF 1e16
+
+const int N = 1e5+5;
+vector<pair<int, int>> graph[N];
+vector<bool> processed(N, false);
+vector<int> cost(N, INF);
+
+void dijkstra(){
+    set<pair<int, int>> s;
+    s.insert({0, 1});
+    cost[1] = 0;
+
+    while(!s.empty()){
+        auto [d, u] = *s.begin();
+        s.erase(s.begin());
+
+        if(processed[u]) continue;
+
+        processed[u] = true;
+        for(auto [v, wt]: graph[u]){
+            if(cost[u] + wt < cost[v]){
+                cost[v] = cost[u] + wt;
+                s.insert({cost[v], v});
+            }
+        }
+    }
+}
+
+int solve(){
+    int n, m;
+    cin >> n >> m;
+    int a, b, wt;
+    while(m--){ 
+        cin >> a >> b >> wt;
+        // Ignore self loops
+        if(a == b) continue;
+
+        // This graph is Directed Graph
+        graph[a].push_back({b, wt});
+    }
+
+    dijkstra();
+
+    for(int u = 1; u <= n; u++){
+        cout << cost[u] << " ";
+    }
+    
+    return 0;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int t = 1;
+    
+    for(int i = 0; i < t; i++)
+        solve();
+}
